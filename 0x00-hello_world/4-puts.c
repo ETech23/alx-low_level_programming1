904f6b645868d6efba4b708b50c5a910c1e09567
Emmanuel Ogbaa <netpady@gmail.com>
2022-09-09 12:02:17 -0700

    The printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+* Main-print the string in the put function
+*Return :0
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
