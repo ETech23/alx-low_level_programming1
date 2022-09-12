aba0e000e045c2f07c5a647d07b8ae9c981c2817
Emmanuel Ogbaa <netpady@gmail.com>
2022-09-12 04:23:29 -0700

    Prints lowercase alphabet z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints lower case alphabets z-a
+ * Return: 0 success
+ */
+int main(void)
+{
+	char x = 'z';
+
+	while (x >= 'a')
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
